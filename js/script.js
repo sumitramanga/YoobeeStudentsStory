c4f1ca94d4d9f61cfa1d6e47631855db7fcc0d9c
Sumitra Manga <sumitramanga@gmail.com>
2018-08-17 17:47:09 +1200

    Filtering system for pie chart works

--- js/script.js
+++ js/script.js
@@ -37,10 +37,14 @@
     }
   ];
 
-  // DOM queries
 
+  // Grabbing the buttons from the html
   var femaleEmployBtn = document.getElementById('femaleEmployBtn');
+  var maleEmployBtn = document.getElementById('maleEmployBtn');
+  var nonBinaryEmployBtn = document.getElementById('nonBinaryEmployBtn');
 
+
+  // Scroll to sections
   $( document ).ready(function() {
     $('#icon').click(function(){
       console.log('working');
@@ -57,6 +61,8 @@
     });
   });
 
+
+// Calling an ajax request to get the json data
   $.ajax({
         url: 'data/data.json',
         dataType: 'json',
@@ -74,6 +80,8 @@
         }
   }); // ajax Ends
 
+
+// To show a graph with data on load
   function addToDefaultArray(){
     for(var i = 0; i < myData.length; i++){
       if (myData[i].job == 'yes'){
@@ -86,6 +94,8 @@
     console.log(defaultArray);
   }
 
+
+  // Filtering the pie chart to genders
   function addToArrays(){
     for(var i = 0; i < myData.length; i++){
       if (myData[i].gender == 'female' && myData[i].job == 'yes'){
@@ -102,16 +112,15 @@
         nonBiArray[1].unemployed.push(myData[i]);
       }
 
-    }
-    console.log('Female Array:');
-    console.log(femaleArray);
-  }
+    } // for loop ends
+  } // addToArray ends
+
 
   google.charts.load('current', {'packages':['corechart', 'bar']});
-
   google.charts.setOnLoadCallback(drawBarChart);
 
-  // Adding event listeners to the buttons for filtering
+
+  // Adding event listeners to the buttons for filtering through genders of pie chart
   femaleEmployBtn.addEventListener("click", function(){
       initPieChart(femaleArray);
   }, false);
@@ -124,6 +133,9 @@
       initPieChart(nonBiArray);
   }, false);
 
+
+  // Function that initialises the pie chart once the json data has been successfully retrieved
+  // An array is passed through as an argument to define what data will be used in the chart, making it dynamic
   function initPieChart(selectedArray){
     google.charts.setOnLoadCallback(drawPieChart);
 
@@ -144,6 +156,7 @@
       chart.draw(data, options);
     }
   } // End initPieChart
+
 
   function drawBarChart() {
       var data = google.visualization.arrayToDataTable([
